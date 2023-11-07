9796d8dbb38cd25aa3f578abba7a7b749c70d003
Abdon LC <skener13@gmail.com>
2023-11-07 12:56:36 -0600

    Subiendo el proyecto a git

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,34 @@
+const Pacientes = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl ">
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {""}
+
+          <span className="font-normal normal-case ">Hook</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {""}
+
+          <span className="font-normal normal-case ">Wilson</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">Email: {""}
+
+          <span className="font-normal normal-case ">wilson@gmail.com</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {""}
+
+          <span className="font-normal normal-case ">10 de Diciembre del 2023</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: {""}
+
+          <span className="font-normal normal-case ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus optio, minima ipsum nisi expe
+          dita animi reiciendis quia vero quam et quisquam aut! Asperiores rem adipisci cumque et suscipit reiciendis dolorem.</span>
+        </p>
+
+      </div>
+  )
+}
+
+export default Pacientes
